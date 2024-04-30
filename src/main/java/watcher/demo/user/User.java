695de45b41231805398b39908d29896b5c36ed46
Yehdar <p.radhey06@gmail.com>
2024-04-30 12:09:29 -0400

    backend: define user model with JPA annotations

---
+++ src/main/java/watcher/demo/user/User.java
@@ -0,0 +1,43 @@
+package watcher.demo.user;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  @Column(name = "name")
+  private String name;
+
+  @Column(name = "email")
+  private String email;
+
+  // Getter and Setter Methods
+  public Long getId() {
+    return this.id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getEmail() {
+    return this.email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+}

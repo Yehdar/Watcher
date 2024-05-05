f38c64c777d20bad8743d62995235cc6dc45e128
Yehdar <p.radhey06@gmail.com>
2024-05-05 17:42:34 -0400

    frontend: add POST request

--- frontend/src/components/UserInterface.tsx
+++ frontend/src/components/UserInterface.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import CardComponent from "./CardComponent";
 
-interface Card {
+interface User {
   id: number;
   name: string;
   email: string;
@@ -51,6 +51,21 @@ const UserInterface: React.FC<UserInterfaceProps> = ({ backendName }) => {
     fetchData();
   }, [backendName, apiURL]);
 
+  // create user
+  const createUser = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const response = await axios.post(
+        `${apiURL}/api/${backendName}/users`,
+        newUser
+      );
+      setUsers([response.data, ...users]);
+      setNewUser({ name: "", email: "" });
+    } catch (error) {
+      console.error("erro making user: ", error);
+    }
+  };
+
   return (
     <div
       className={`user-interface ${bgColor} ${backendName} w-full max-w-md p-4 my-4 rounded shadow`}
@@ -63,6 +78,32 @@ const UserInterface: React.FC<UserInterfaceProps> = ({ backendName }) => {
       <h2 className="text-xl front-bold text-center text-black mb-6">{`${
         backendName.charAt(0).toUpperCase() + backendName.slice(1)
       } Backend`}</h2>
+
+      {/* create em */}
+      <form
+        onSubmit={createUser}
+        className="mb-6 p-4 bg-blue-100 rounded shadow"
+      >
+        <input
+          placeholder="Name"
+          value={newUser.name}
+          onChange={(e) => setNewUser({ ...newUser, name: e.target.value })}
+          className="mb-2 w-full p-2 border-gray-300 rounded"
+        />
+        <input
+          placeholder="Email"
+          value={newUser.email}
+          onChange={(e) => setNewUser({ ...newUser, email: e.target.value })}
+          className="mb-2 w-full p-2 border-gray-300 rounded"
+        />
+        <button
+          type="submit"
+          className="w-full p-2 text-white bg-blue-500 rounded hover:bg-blue-600"
+        >
+          Add User
+        </button>
+      </form>
+
       {/* display em */}
       <div className="space-y-4">
         {users.map((user) => (
